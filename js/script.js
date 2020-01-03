946d59a06d42c98357e8a0802458b7676e23611b
mdraza <raza.uidev@gmail.com>
2020-01-03 15:58:49 +0530

    Initial Commit

--- js/script.js
+++ js/script.js
@@ -1,21 +1,31 @@
+/****  Sticky Navbar  ****/
+$(document).ready(function () {
+    $(window).on("scroll", function () {
+        var t = $(".navbar");
+        $(window).scrollTop() >= t.height() ? t.addClass("nav-scroll") : t.removeClass("nav-scroll")
+    })
+});
+
+
+
 /****  Owl Carousel  ****/
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    autoplay:true,
-    autoplayTimeout:2000,
-    dots:false,
-    autoplayHoverPause:false,
-    responsive:{
-        0:{
-            items:1
+    loop: true,
+    margin: 10,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    dots: false,
+    autoplayHoverPause: false,
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:3
+        600: {
+            items: 3
         },
-        1000:{
-            items:4
+        1000: {
+            items: 5
         }
     }
 })
