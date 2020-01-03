59766084c89f8b5b3c65019ae92c737af1ad33e2
mdraza <raza.uidev@gmail.com>
2020-01-03 12:46:50 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/****  Owl Carousel  ****/
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:2000,
+    dots:false,
+    autoplayHoverPause:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
