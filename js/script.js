bf2e0602f4eedaa4b9c337185587f30aaf916f32
mdraza <raza.uidev@gmail.com>
2020-01-06 16:01:49 +0530

    initial commit

--- js/script.js
+++ js/script.js
@@ -47,3 +47,22 @@ $('.owl-carouselTwo').owlCarousel({
         }
     }
 })
+/****  Testimonial Carousel  ****/
+$('.owl-carouselThree').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    navText: ["<i class='icofont-long-arrow-left'></i>","<i class='icofont-long-arrow-right'></i>"],
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
